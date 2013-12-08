e945a9ba71458659d2bf848b74209d3f81f6b151
bengle <qiyou.xj@taobao.com>
2013-12-08 22:25:48 +0800

    add index

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/phone-list', routes.list);
+app.get('/phone-detail', routes.detail);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,10 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+exports.list = function(req, res){
+    //console.log('list');
+    res.render('phone-list', { title: 'Express' });
+};
+exports.detail = function(req, res){
+    res.render('phone-detail', { title: 'Express' });
+};
